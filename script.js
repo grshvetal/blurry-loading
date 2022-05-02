c143287928d8481cba81f80abcd6897b6367c955
Shvetal Gamit <72142454+grshvetal@users.noreply.github.com>
2022-05-02 19:15:50 +0530

    Rename variables and functions

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const loadText = document.querySelector('.loading-text')
 
 let load = 0
 
-let intervalID = setInterval(loadPlus, 30)
+let int = setInterval(blurring, 30)
 
-function loadPlus() {
+function blurring() {
     load++
-     if (load >= 100) {
-        clearInterval(intervalID)
+     if (load > 99) {
+        clearInterval(int)
     }
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
